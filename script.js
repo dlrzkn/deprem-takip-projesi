bb18325b154defd803f21ba015248969d25a18f7
Dilara Özkan <85346535+dlrzkn@users.noreply.github.com>
2026-03-06 22:01:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ async function updateQuakes() {
 function rotateGlobe() {
     if (spinEnabled && !userInteracting && map.getZoom() < 5) {
         const center = map.getCenter();
-        center.lng -= 0.05;
+        center.lng -= 0.1;
         map.easeTo({ center, duration: 2000, easing: (t) => t });
     }
 }
