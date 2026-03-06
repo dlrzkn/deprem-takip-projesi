bfb42d80c7157160b64b819f4b004ed326ddc9aa
Dilara Özkan <85346535+dlrzkn@users.noreply.github.com>
2026-03-06 22:17:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -60,8 +60,23 @@ async function updateQuakes() {
                 type: 'circle',
                 source: 'usgs',
                 paint: {
-                    'circle-radius': ['interpolate', ['linear'], ['get', 'mag'], 1, 2, 3, 5, 5, 12, 7, 25],
-                    'circle-color': ['step', ['get', 'mag'], '#00ff00', 2.5, '#ffff00', 4.5, '#ffa500', 6.0, '#ff0000'],
+                    'circle-radius': [
+    'interpolate', ['linear'], ['get', 'mag'],
+    1, 2,
+    3, 4,
+    5, 8,
+    7, 15,
+    9, 30 // 9 ve üzeri için devasa ama ekranı kaplamayan bir boyut
+],
+'circle-color': [
+    'step', ['get', 'mag'],
+    '#00ff00', 2.5, 
+    '#ffff00', 4.5, 
+    '#ffa500', 6.0, 
+    '#ff0000', 8.0, 
+    '#8b0000'  // 8.0 ve üzeri (Mega Deprem) için koyu bordo
+],
+
                     'circle-opacity': 0.7,
                     'circle-stroke-width': 1,
                     'circle-stroke-color': '#ffffff'
